a4f19842bfb4d22dde262ecaf1193063003cbe86
temirlans1 <temirlan.smagulov@nu.edu.kz>
2020-06-03 02:53:05 +0600

    homePage mobile

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row } from 'reactstrap';
+import arrowDown from '../icons/arrowDown.png';
 
 class Home extends Component {
   render() {
@@ -7,12 +8,33 @@ class Home extends Component {
       <div id="homeSection" className="mySection">
         <Container fluid>
           <Row>
-            <div className="col-12 col-md-6">
-              <div id="hpGameCard1"></div>
+            <div className="col-12 col-md-6" id="cardsGroup">
+              <div className="gameCard type21">
+                <div className="innerCard">
+                  <h2 className="cardNumber firstCard">21</h2>
+                </div>
+              </div>
+              <div className="gameCard type20">
+                <div className="innerCard">
+                  <h2 className="cardNumber secondCard">20</h2>
+                </div>
+              </div>
+              <div className="gameCard type19">
+                <div className="innerCard">
+                  <h2 className="cardNumber thirdCard">19</h2>
+                </div>
+              </div>
             </div>
-            <div className="col-12 col-md-6">
-              <h4>Text</h4>
+            <div className="col-12 col-md-6" id="homePageText">
+              <div className="text1">test your mind</div>
+              <div className="text2">make as few attempts as possible</div>
             </div>
+          </Row>
+          <Row>
+            <a className="toRules" href="#rulesSection">
+              <p>go</p>
+              <img src={arrowDown}></img>
+            </a>
           </Row>
         </Container>
       </div>
